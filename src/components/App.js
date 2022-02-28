3c8fcd5fa7acb49cf29d05c7b0e12d4bc0ca4458
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2022-02-28 14:32:57 -0800

    Add Read all functionality - app displays all users in the Firestore database

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import './../App.css';
+import { useState, useEffect } from 'react';
+import db from './../firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionReference = collection(db, "users")
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionReference);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+    }
+
+    getUsers()
+
+  }, [])
+  
   return (
     <div className="App">
-      
+      {users.map((user) => {
+        return (
+        <div>
+          {" "}
+          <h1>Name: {user.name}</h1> 
+          <h1>Age: {user.age}</h1> 
+        </div>
+        )})}
     </div>
   );
 }
